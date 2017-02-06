efe1d8e32d778421e370fb9c76c41e4cf77b7b6d
iandry <ian_rak@yahoo.fr>
2017-02-06 17:18:22 +0100

    jeudememoire

--- js/script.js
+++ js/script.js
@@ -4,11 +4,33 @@ var boardElement = document.getElementById("board");
 
 for (var i = 0; i < cardsNumber/2; i++){
     var cardElement = document.createElement ("div");
+    var cacheElement = document.createElement ("div");
+
     cardElement.innerHTML = i;
+
     cardElement.className = "card";
+    cacheElement.className = "cache";
+
     boardElement.appendChild(cardElement);
+    cardElement.appendChild(cacheElement);
+
+
+
+
+
     var cardElement = document.createElement ("div");
+    var cacheElement = document.createElement ("div");
+
     cardElement.innerHTML = i;
+
     cardElement.className = "card";
+    cacheElement.className = "cache";
+
     boardElement.appendChild(cardElement);
-    }
+    cardElement.appendChild(cacheElement);
+}
+
+
+    // cacheElement.addEventListener("click",function(){
+    //     document.getElementByClassName('cache').style.visibility='hidden';
+    // }
