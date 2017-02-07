339f11c377c392273f95f213a9d0c70386e84f1a
Yo <yohanndore48@gmail.com>
2017-02-07 10:57:10 +0100

    version 3 du jeu de mémoire

--- js/script.js
+++ js/script.js
@@ -1,36 +1,32 @@
-var cardsNumber = 12;
-var boardElement = document.getElementById("board");
 
+var boardElement = document.getElementById("board");
+var cardsNumber = 12;
 
 for (var i = 0; i < cardsNumber/2; i++){
-    var cardElement = document.createElement ("div");
-    var cacheElement = document.createElement ("div");
+    for (var j = 0; j < 2; j++) {
+        var cardElement = document.createElement ("div");
+        var cacheElement = document.createElement ("div");
 
-    cardElement.innerHTML = i;
+        cardElement.innerHTML = i;
 
-    cardElement.className = "card";
-    cacheElement.className = "cache";
+        cardElement.className = "card";
+        cacheElement.className = "cache";
 
-    boardElement.appendChild(cardElement);
-    cardElement.appendChild(cacheElement);
+        boardElement.appendChild(cardElement);
+        cardElement.appendChild(cacheElement);
 
+        cacheElement.addEventListener("click",function(){
 
+            if (this.style.display == "none"){
+                console.log("je montre");
+                this.style.display = "block";
 
-
-
-    var cardElement = document.createElement ("div");
-    var cacheElement = document.createElement ("div");
-
-    cardElement.innerHTML = i;
-
-    cardElement.className = "card";
-    cacheElement.className = "cache";
-
-    boardElement.appendChild(cardElement);
-    cardElement.appendChild(cacheElement);
+            }
+            else {
+                console.log("je cache");
+                this.style.display = "none";
+            }
+            
+        });
+    }
 }
-
-
-    // cacheElement.addEventListener("click",function(){
-    //     document.getElementByClassName('cache').style.visibility='hidden';
-    // }
